65de7678f811bc0ef2e99c0aaa19f23262bac119
Jordan J <jordanr2m@gmail.com>
2024-02-13 12:23:31 -0800

    Refactored JS code.

---
+++ js/practice.js
@@ -0,0 +1,75 @@
+const submitButton = document.querySelector("#submit");
+const firstName = document.querySelector("#fname");
+const lastName = document.querySelector("#lname");
+const emailAddress = document.querySelector("#email");
+const password = document.querySelector("#password");
+
+const errorIcons = Array.from(document.querySelectorAll(".error-icon"));
+// console.log(errorIcons);
+const errorMsgs = Array.from(document.querySelectorAll(".error-message"));
+
+
+submitButton.addEventListener("click", function (e) {
+    const acceptedNames = /^[a-zA-Z ]{2,30}$/;
+    const fname = firstName.value;
+    const lname = lastName.value;
+
+    const acceptedEmail = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const email = emailAddress.value;
+
+    // const acceptedPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    const pword = password.value;
+
+    if (!fname.match(acceptedNames)) {
+        e.preventDefault();
+        firstName.style.border = "2px solid var(--accent)";
+        addError(errorIcons, 0, errorMsgs, 0);
+
+    } else if (fname.match(acceptedNames) && !lname.match(acceptedNames)) {
+        firstName.style.border = "1px solid var(--border)";
+        removeError(errorIcons, 0, errorMsgs, 0);
+
+        e.preventDefault();
+        lastName.style.border = "2px solid var(--accent)";
+        addError(errorIcons, 1, errorMsgs, 1);
+
+    } else if (lname.match(acceptedNames) && !email.match(acceptedEmail)) {
+        lastName.style.border = "1px solid var(--border)";
+        removeError(errorIcons, 1, errorMsgs, 1);
+
+        e.preventDefault();
+        emailAddress.style.border = "2px solid var(--accent)";
+        addError(errorIcons, 2, errorMsgs, 2);
+
+    } else if (email.match(acceptedEmail) && pword.length === 0) {
+        emailAddress.style.border = "1px solid var(--border)";
+        removeError(errorIcons, 2, errorMsgs, 2);
+
+        e.preventDefault();
+        password.style.border = "2px solid var(--accent)";
+        addError(errorIcons, 3, errorMsgs, 3);
+
+    } else {
+        password.style.border = "1px solid var(--border)";
+
+        errorIcons.forEach(() => this.classList.add("hidden"));
+        errorMsgs.forEach(() => this.classList.add("hidden"));
+    }
+});
+// Could change functionality so that all blanks fields show an alert at once. Do this by using seperate if else statements for each item, rather combining them into one if else statement //
+
+
+//// Functions to Add & Remove Errors /////
+const addError = function (array1, index1, array2, index2) {
+    array1[index1].classList.remove("hidden");
+    array2[index2].classList.remove("hidden");
+};
+
+const removeError = function (array1, index1, array2, index2) {
+    array1[index1].classList.add("hidden");
+    array2[index2].classList.add("hidden");
+};
+
+/* See this code for inspiration on refactoring:
+https://github.com/devenzonascimento/frontend-mentor-pages/blob/main/newbie/intro-component-with-signup-form-master/app.js */
+/* Live site: https://devenzonascimento.github.io/frontend-mentor-pages/newbie/intro-component-with-signup-form-master/index.html */
--- js/script.js
+++ js/script.js
@@ -5,11 +5,15 @@ const emailAddress = document.querySelector("#email");
 const password = document.querySelector("#password");
 
 const errorIcons = Array.from(document.querySelectorAll(".error-icon"));
-// console.log(errorIcons);
 const errorMsgs = Array.from(document.querySelectorAll(".error-message"));
 
+// Function to Add Errors
+function addError (array1, index1, array2, index2) {
+    array1[index1].classList.remove("hidden");
+    array2[index2].classList.remove("hidden");
+};
 
-submitButton.addEventListener("click", function (e) {
+function validateForm (e) {
     const acceptedNames = /^[a-zA-Z ]{2,30}$/;
     const fname = firstName.value;
     const lname = lastName.value;
@@ -24,48 +28,30 @@ submitButton.addEventListener("click", function (e) {
         e.preventDefault();
         firstName.style.border = "2px solid var(--accent)";
         addError(errorIcons, 0, errorMsgs, 0);
-
-    } else if (fname.match(acceptedNames) && !lname.match(acceptedNames)) {
-        firstName.style.border = "1px solid var(--border)";
-        removeError(errorIcons, 0, errorMsgs, 0);
-
+    } 
+    
+    if (!lname.match(acceptedNames)) {
         e.preventDefault();
         lastName.style.border = "2px solid var(--accent)";
         addError(errorIcons, 1, errorMsgs, 1);
-
-    } else if (lname.match(acceptedNames) && !email.match(acceptedEmail)) {
-        lastName.style.border = "1px solid var(--border)";
-        removeError(errorIcons, 1, errorMsgs, 1);
-
+    } 
+    
+    if (!email.match(acceptedEmail)) {
         e.preventDefault();
         emailAddress.style.border = "2px solid var(--accent)";
         addError(errorIcons, 2, errorMsgs, 2);
-
-    } else if (email.match(acceptedEmail) && pword.length === 0) {
-        emailAddress.style.border = "1px solid var(--border)";
-        removeError(errorIcons, 2, errorMsgs, 2);
-
+    } 
+    
+    if (pword.length < 1) {
         e.preventDefault();
         password.style.border = "2px solid var(--accent)";
         addError(errorIcons, 3, errorMsgs, 3);
+    } 
 
-    } else {
-        password.style.border = "1px solid var(--border)";
-
-        errorIcons.forEach(() => this.classList.add("hidden"));
-        errorMsgs.forEach(() => this.classList.add("hidden"));
+    // What to do if a form is submitted blank (no icons show)
+    if (!fname.match(acceptedNames) && !lname.match(acceptedNames) && !email.match(acceptedEmail) && pword.length < 1) {
+        errorIcons.forEach(icon => icon.classList.add('hidden'));
     }
-});
-// Could change functionality so that all blanks fields show an alert at once. Do this by using seperate if else statements for each item, rather combining them into one if else statement //
+}
 
-
-//// Functions to Add & Remove Errors /////
-const addError = function (array1, index1, array2, index2) {
-    array1[index1].classList.remove("hidden");
-    array2[index2].classList.remove("hidden");
-};
-
-const removeError = function (array1, index1, array2, index2) {
-    array1[index1].classList.add("hidden");
-    array2[index2].classList.add("hidden");
-};
+submitButton.addEventListener("click", validateForm);
